a47e464e1c80df675594badef5264cc57021f1c5
Olive568 <99855217+Olive568@users.noreply.github.com>
2024-02-12 22:40:15 +0800

    Add files via upload

--- Valentines.js
+++ Valentines.js
@@ -1,10 +1,11 @@
 function showRadioButtons() {
     document.getElementById("mainContent").style.display = "none"; // Hide main content
     document.getElementById("radioButtons").style.display = "block"; // Show radio buttons
+    document.getElementById("obey").style.display = "none"
 }
 
 function submitReasons() {
-    var reasons = document.querySelectorAll('input[name="reason"]:checked');
+    /*var reasons = document.querySelectorAll('input[name="reason"]:checked');
     var selectedReasons = [];
     reasons.forEach(function(checkbox) {
         selectedReasons.push(checkbox.value);
@@ -14,5 +15,21 @@ function submitReasons() {
 
     var mailtoLink = "mailto:luislabapis@gmail.com?subject=Valentine's Day Reasons&body=" + encodeURIComponent(emailBody);
 
-    window.location.href = mailtoLink;
+    window.location.href = mailtoLink;*/
+    document.getElementById("radioButtons").style.display = "none";
+    document.getElementById("dates").style.display = "block";
+}
+function showNoPopup() {
+    document.getElementById("mainContent").style.display = "none";
+    document.getElementById("sorry").style.display = "block";
+}
+function showNoPopup2()
+{
+    document.getElementById("sorry").style.display = "none";
+    document.getElementById("obey").style.display = "block"
+}
+function showYesPopup()
+{
+    document.getElementById("sorry").style.display = "none";
+    document.getElementById("please").style.display = "block"
 }
